cbf9aba0d53742eb67798047cf94554dc54db451
Erica Ho <researcher.erica@gmail.com>
2022-10-25 22:26:54 +1100

    Calculates digit sum by recursion; simplified the code by performing arithmetic operations on vectors; clarified the code.

--- luhn-checksum.R
+++ luhn-checksum.R
@@ -1,36 +1,37 @@
+# A recursive function that calculates the digit sum for a given number n 
+digitsum <- function(n){
+  if (n < 10) { return (n) }
+  else { return ((n %% 10) + digitsum(floor( n / 10 ))) }
+}
+
 # Calculates the Luhn checksum for a given number x (double the value of every other digit from right to left by default)
 checksum <- function(x,double_position=""){
   if (double_position == "odd") { double_position_remainder <- 1 }
   else if (double_position == "even") { double_position_remainder <- 0 }
   else { double_position_remainder <- nchar(x) %% 2 }
   
-  doubling_array <- c()
-  remaining_array <- c()
+  doubling_digits <- c()
+  remaining_digits <- c()
   
   for (i in 1:nchar(x)){
-    if (i %% 2 == double_position_remainder) { doubling_array <- c(doubling_array, as.numeric(substr(x, i, i))) }
-    else { remaining_array <- c(remaining_array, as.numeric(substr(x, i, i))) }
+    if (i %% 2 == double_position_remainder) { doubling_digits <- c(doubling_digits, as.numeric(substr(x, i, i))) }
+    else { remaining_digits <- c(remaining_digits, as.numeric(substr(x, i, i))) }
   }
+
+  doubling_digits <- 2 * doubling_digits
+  # sapply will apply a function to all the elements of the input list/vector/df, and then output a vector/matrix
+  doubling_digits <- sapply(doubling_digits, digitsum)
+
+  all_digits <- c(doubling_digits, remaining_digits)
+  all_digits_sum <- sum(all_digits)
   
-  for (i in 1:length(doubling_array)){
-    result <- 2 * doubling_array[i]
-    if (result >= 10) { result <- (result %% 10) + 1 }
-    doubling_array[i] <- result
-  }
-  
-  merged_array <- c(doubling_array, remaining_array)
-  sum <- 0
-  for (i in 1:length(merged_array)){
-    sum <- sum + merged_array[i]
-  }
-  
-  return ((10 - (sum %% 10)) %% 10)
+  return ((10 - (all_digits_sum %% 10)) %% 10)
 }
 
 # Examples
-checksum("462474823324978")
-checksum("7992739871")
-checksum("7992739871",double_position="odd")
-checksum("546046280340313")
-checksum("492990365278615")
+checksum("462474823324978") # 0
+checksum("7992739871") # 3
+checksum("7992739871",double_position="odd") # 4
+checksum("546046280340313") # 2
+checksum("492990365278615") # 8
 
